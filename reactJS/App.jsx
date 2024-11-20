5478be102aec5f11780a00c8a6d3d4f3feb2bd76
fakhruls-here <fakhrulishere@gmail.com>
2024-11-20 13:13:38 +0600

    react semantic boilerplate code added here for best seo perfomance

---
+++ reactJS/App.jsx
@@ -0,0 +1,23 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <p>header</p>
+        <nav>navbar</nav>
+      </header>
+      <main>
+        <p>main</p>
+        <section>section</section>
+        <article>
+          <p>article</p>
+          <figure>figure</figure>
+          <p>para</p>
+        </article>
+        <aside>aside</aside>
+      </main>
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default App;
